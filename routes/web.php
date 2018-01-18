276d060a8fc9e0df243b1d3208e605f4d4dae49a
minhbeo1997 <you@example.com>
2018-01-18 18:15:57 +0700

    fix Route

--- routes/web.php
+++ routes/web.php
@@ -4,22 +4,21 @@ Route::group(['namespace' => 'Admin', 'prefix' => 'admin'], function () {
     Route::post('/login', 'AdminController@postlogin');
     Route::get('/logout', 'AdminController@logoutAdmin')->name('log_Out_Admin');
     Route::group(['middleware' => 'admin', 'prefix' => 'home'], function () {
-        Route::get('/update/score', 'ManageStudentController@indexPoint')->name('update_score');
+        Route::get('/update-score', 'ManageStudentController@indexPoint')->name('update_score');
         Route::get('/', 'AdminController@home')->name('homeAdmin');
         Route::get('/info', 'ManageStudentController@index')->name('info_Member');
         Route::get('/info/sreach', 'ManageStudentController@index')->name('sreach_student');
         Route::put('/info/{id}', 'ManageStudentController@update')->name('update_student');
-        Route::get('/update/info', 'AdminController@updateInfo')->name('update_info_Member');
-        Route::get('/update/score/sreach', 'ManageStudentController@indexPoint')->name('sreach_score');
-        Route::put('/update/score/{id}', 'ManageStudentController@updatePoint')->name('update_point');
-        Route::post('/update/score', 'ManageStudentController@storePoint')->name('create_point');
+        Route::get('/update-info', 'AdminController@updateInfo')->name('update_info_Member');
+        Route::get('/update-score/search', 'ManageStudentController@indexPoint')->name('sreach_score');
+        Route::put('/update-score/{id}', 'ManageStudentController@updatePoint')->name('update_point');
+        Route::post('/update-score', 'ManageStudentController@storePoint')->name('create_point');
         Route::get('/create/student', 'ManageStudentController@view')->name('get_create_student');
         Route::get('/create/teacher', 'ManageTeacherController@view')->name('get_create_teacher');
         Route::post('/create/teacher', 'ManageTeacherController@store')->name('create_teacher');
         Route::post('/create/student', 'ManageStudentController@store')->name('create_student');
-        Route::get('/update/class', 'ClassController@index')->name('update_class');
-        Route::post('/update/class/{id}', 'ClassController@destroy')->name('destroy_class_admin');
-        Route::put('/update/class/{id}', 'ClassController@Update')->name('update_class_admin');
+        Route::get('/update-class', 'ClassController@index')->name('update_class');
+        Route::put('/update-class/{id}', 'ClassController@Update')->name('update_class_admin');
         Route::post('/create/class', 'ClassController@store')->name('create_class');
         Route::get('/check/point', 'AdminController@indexPick')->name('admin_check_point');
         Route::get('/check/point/search', 'AdminController@searchPoint')->name('search_statistic_score');
@@ -34,9 +33,9 @@ Route::group(['namespace' => 'Student', 'prefix' => 'student'], function (){
     Route::get('verify/{token}', 'StudentController@verify')->name('student.verify');
     Route::group(['middleware' => 'student', 'prefix' => 'home'], function () {
         Route::get('/', 'StudentController@homeStudent')->name('home_student');
-        Route::get('/pick/semester', 'StudentController@indexSemester')->name('index_semester_student');
-        Route::get('/pick/semester/{id}', 'StudentController@pickSemester')->name('pick_semester_student');
-        Route::put('/update/class/{id}', 'StudentController@UpdateClass')->name('update_class_student');
+        Route::get('/pick-semester', 'StudentController@indexSemester')->name('index_semester_student');
+        Route::get('/pick-semester/{id}', 'StudentController@pickSemester')->name('pick_semester_student');
+        Route::put('/update-class/{id}', 'StudentController@UpdateClass')->name('update_class_student');
     });
 });
 Route::group(['namespace' => 'Teacher', 'prefix' => 'teacher'], function (){
@@ -48,10 +47,10 @@ Route::group(['namespace' => 'Teacher', 'prefix' => 'teacher'], function (){
         Route::get('/', 'TeacherController@homeTeacher')->name('home_teacher');
         Route::get('/update/score', 'TeacherController@indexPick')->name('teacher_update_score');
         Route::get('/info/{id}', 'TeacherController@pickClass')->name('pick_class');
-        Route::put('/update/score/{id}', 'TeacherController@UpdatePoint')->name('update_point_teacher');
-        Route::get('/update/class', 'TeacherController@indexClass')->name('teacher_class');
-        Route::put('/update/class/{id}', 'TeacherController@UpdateClass')->name('update_class_teacher');
-        Route::get('/pick/semester', 'TeacherController@indexSemester')->name('pick_semester');
-        Route::get('/pick/semester/{id}', 'TeacherController@pickSemester')->name('pick_semester_teacher');
+        Route::put('/update-score/{id}', 'TeacherController@UpdatePoint')->name('update_point_teacher');
+        Route::get('/update-class', 'TeacherController@indexClass')->name('teacher_class');
+        Route::put('/update-class/{id}', 'TeacherController@UpdateClass')->name('update_class_teacher');
+        Route::get('/pick-semester', 'TeacherController@indexSemester')->name('pick_semester');
+        Route::get('/pick-semester/{id}', 'TeacherController@pickSemester')->name('pick_semester_teacher');
     });
 });
